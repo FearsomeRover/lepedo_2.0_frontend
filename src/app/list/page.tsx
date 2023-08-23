76262f8e5347e4503ca6da787c8c1b4d5b794353
FearsomeRover <bujgergo@gmail.com>
2023-08-23 13:12:00 +0200

    did some css refactoring

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,46 +1,19 @@
 "use client"
 import axios from "axios";
-import "../components/List/list.css"
+import styles from "./page.module.css"
 import Expense from "@/components/List/Expense/Expense";
 import Transfer from "@/components/List/Transfer/Transfer";
+
 import { useState, useEffect } from "react";
+import { ExpenseType } from "@/types/expense";
 
 export default function ListPage() {
-  const [expenses, setExpenses] = useState(false);
-  const [transfers, setTransfers] = useState(false);
-  const [noExpense, setNoExpense] = useState(false);
-  const [noTransfer, setNoTransfer] = useState(false);
   
-  const refreshExpenses = () =>{
-    const getExpenses = async () =>{
-      const response = await axios.get(
-        process.env.REACT_APP_BASE_URL + "/expense"
-      );
-      const data = await response.data;
-      if(data.length===0) setNoExpense(true);
-      setExpenses(data);
-    }
-    getExpenses();
-  }
-  const refreshTransfers = () => {
-    const getTransfers = async () => {
-      const response = await axios.get(
-        process.env.REACT_APP_BASE_URL + "/transfer"
-      );
-      const data = await response.data;
-      if(data.length===0) setNoTransfer(true);
-      setTransfers(data);
-    };
-    getTransfers();
-  }
-  useEffect(() => {
-    refreshExpenses();
-    refreshTransfers();
-  }, []);
+  
   return (
-    <div>
-      {(expenses)?<Expense expenses={expenses} refresh={refreshExpenses}/>:noExpense?<h3>No expenses yet</h3>:<p>loading</p>}
-      {(transfers)?<Transfer transfers={transfers} refresh={refreshTransfers}/>:noTransfer?<h3>No transfers yet</h3>:<p>loading</p>}
-    </div>
+    <>
+      <Expense/>
+      {(transfers)?<Transfer/>:noTransfer?<h3>No transfers yet</h3>:<p>loading</p>}
+    </>
   );
 }
