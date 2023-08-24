f6df2fd163070c00880860ca875de40725abbcd7
FearsomeRover <bujgergo@gmail.com>
2023-08-24 13:10:58 +0200

    icon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body className={inter.className}>
         <Header />
         {children}
