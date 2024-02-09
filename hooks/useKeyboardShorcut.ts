daa75ffd6270c215047e867d816e28e68821d2bb
mozsarmate <mozsarmatee@gmail.com>
2024-02-09 13:42:59 +0100

    shortcuts without Ctrl control

--- hooks/useKeyboardShorcut.ts
+++ hooks/useKeyboardShorcut.ts
@@ -1,8 +1,10 @@
-import { KeyboardEvent, useEffect } from 'react'
+import { KeyboardEvent, useEffect, useState } from 'react'
 
-type Key = 'ctrl' | 'shift' | 'alt' | string
+type Key = 'ctrl' | 'shift' | 'alt' | 'esc' | string
 
 export const useKeyboardShortcut = (keys: Key[], callback: () => void) => {
+    /*    const [ctrlPressed, setCtrlPressed] = useState(false)*/
+
     useEffect(() => {
         const handleKeyDown = (event: KeyboardEvent) => {
             const targetTagName = (
@@ -18,28 +20,46 @@ export const useKeyboardShortcut = (keys: Key[], callback: () => void) => {
             if (event.ctrlKey || event.altKey || event.metaKey) {
                 event.preventDefault()
             }
+            /*
+            if (event.ctrlKey && !ctrlPressed) {
+                setCtrlPressed(true)
+            }
+*/
 
             if (
                 keys.every(
                     (key) =>
                         (key === 'ctrl' && event.ctrlKey) ||
+                        /*                        ctrlPressed ||*/
                         (key === 'shift' && event.shiftKey) ||
                         (key === 'alt' && event.altKey) ||
+                        (key === 'esc' && event.key === 'Escape') ||
                         (typeof key === 'string' &&
                             event.key.toLowerCase() === key),
                 )
             ) {
                 callback()
-                event.preventDefault() // Prevent default browser shortcuts
+                event.preventDefault() // Prevent default browser behavior
             }
         }
 
+        /*        const handleKeyUp = (event: KeyboardEvent) => {
+            if (event.ctrlKey && ctrlPressed) {
+                setCtrlPressed(false)
+            }
+        }
+        console.log('ctrl ' + ctrlPressed)*/
+
         // @ts-ignore
         window.addEventListener('keydown', handleKeyDown)
+        /*        // @ts-ignore
+        window.addEventListener('keyup', handleKeyUp)*/
 
         return () => {
             // @ts-ignore
             window.removeEventListener('keydown', handleKeyDown)
+            /*            // @ts-ignore
+            window.removeEventListener('keyup', handleKeyUp)*/
         }
     }, [keys, callback])
 }
