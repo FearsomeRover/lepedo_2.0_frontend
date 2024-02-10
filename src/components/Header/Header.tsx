e175f6aef6b5322397d1914301b0f7fd391de23c
mozsarmate <mozsarmatee@gmail.com>
2024-02-10 11:43:40 +0100

    Beauty highlighting of selected tab

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,36 @@
-import styles from './header.module.css'
+'use client'
 import Link from 'next/link'
 import Cards from '@/components/Header/Cards'
+import { usePathname } from 'next/navigation'
+
+type headerLink = {
+    title: string
+    address: string
+}
+
 export default function Header() {
+    const headerLinks: headerLink[] = [
+        { title: 'Összegzés', address: '/' },
+        { title: 'Tételek', address: '/expenses' },
+        { title: 'Utalások', address: '/transfers' },
+        { title: 'QR kódok', address: '/qrs' },
+    ]
+    const curPathname = usePathname()
+
     return (
         <div>
             <h1>Lepedő</h1>
 
-            <Link href="/">
-                <h2>Összegzés</h2>
-            </Link>
-            <Link href="/expenses">
-                <h2>Tételek</h2>
-            </Link>
-            <Link href="/transfers">
-                <h2>Utalások</h2>
-            </Link>
-            <Link href="/qrs">
-                <h2>QR kódok</h2>
-            </Link>
+            {headerLinks.map((link, index) => (
+                <Link key={index} href={link.address}>
+                    <h2
+                        className={
+                            curPathname === link.address ? 'activeheader' : ''
+                        }>
+                        {link.title}
+                    </h2>
+                </Link>
+            ))}
             <Cards summary={true} />
         </div>
     )
