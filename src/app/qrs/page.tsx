21568864c574bf5bfc97252590a11017fb1ab4d4
mozsarmate <mozsarmatee@gmail.com>
2024-02-29 19:34:14 +0100

    i cannot hide that arrow fml

---
+++ src/app/qrs/page.tsx
@@ -0,0 +1,25 @@
+import QRCard from '@/components/QR/QRCard'
+
+const dummyUser = {
+    name: 'John Doe',
+    color: 'coral',
+    revTag: '1234',
+    id: '1234',
+}
+
+export default function page() {
+    return (
+        <>
+            <QRCard title={'beer'} id={'fdsa'} userTo={dummyUser} amount={400} />
+            <QRCard title={'sajt'} id={'23'} userTo={dummyUser} amount={1200} />
+            <QRCard title={'sajt'} id={'23'} userTo={dummyUser} amount={1200} />
+            <QRCard title={'sajt'} id={'23'} userTo={dummyUser} amount={1200} />
+            <QRCard title={'sajt'} id={'23'} userTo={dummyUser} amount={1200} />
+            <QRCard title={'sajt'} id={'23'} userTo={dummyUser} amount={1200} />
+            <QRCard title={'sajt'} id={'23'} userTo={dummyUser} amount={1200} />
+            <QRCard title={'sajt'} id={'23'} userTo={dummyUser} amount={1200} />
+            <QRCard title={'sajt'} id={'23'} userTo={dummyUser} amount={1200} />
+            <QRCard title={'sajt'} id={'23'} userTo={dummyUser} amount={1200} />
+        </>
+    )
+}
---
+++ src/types/qr.ts
@@ -0,0 +1,8 @@
+import { BasicUser } from '@/types/user'
+
+export type QrType = {
+    id: string
+    title: string
+    amount: number
+    userTo: BasicUser
+}
